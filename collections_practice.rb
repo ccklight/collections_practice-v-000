05efab0c101c27a0b65de441f728a9f33ad6cf12
Cheryl K <cherylkelson@icloud.com>
2018-09-18 20:52:07 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -37,6 +37,19 @@ def kesha_maker(array)
 end
 
 
+def find_a(array)
+ array.find_all do |word|
+   word[0] == "a"
+ end
+
+
+
+ # using select method
+   # array.select do |word|
+   #   word[0] == "a"
+   # end
+end
+
 
 
 
