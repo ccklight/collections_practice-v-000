ee2fdb6000568cb69a95b4e19701b14abe99a54a
Cheryl K <cherylkelson@icloud.com>
2018-09-18 20:56:00 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -17,12 +17,10 @@ def sort_array_char_count(array)
 end
 
 
-
 def swap_elements(array)
  array[1], array[2] = array[2], array[1]
  array
 end
-
 
 
 def reverse_array(array)
@@ -33,7 +31,7 @@ end
 def kesha_maker(array)
  array.each do |item|
    item[2] = "$"
- end
+  end
 end
 
 
@@ -41,9 +39,8 @@ def find_a(array)
  array.find_all do |word|
    word[0] == "a"
  end
+end
 
-
- 
 
 def sum_array(array)
  sum = 0
@@ -51,7 +48,6 @@ def sum_array(array)
    sum+=num
     end
   sum
-
 end
 
 
